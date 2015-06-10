f5a92e617c68f48bb4349053ce3781c73bd19ec1
vinaypuppal <puppalvinay@gmail.com>
2015-06-11 01:30:41 +0530

    Accounts system fixed redirections and added dummy wizard

--- server/permissions.js
+++ server/permissions.js
@@ -1,8 +1,8 @@
-Accounts.validateLoginAttempt(function(attempt){
-  if (attempt.user && attempt.user.emails && !attempt.user.emails[0].verified ) {
-    console.log('email not verified');
-
-    return false; // the login is aborted
-  }
-  return true;
-});
+// Accounts.validateLoginAttempt(function(attempt){
+//   if (attempt.user && attempt.user.emails && !attempt.user.emails[0].verified ) {
+//     console.log('email not verified');
+//     Router.go('emailVerifyMsg');
+//     return false; // the login is aborted
+//   }
+//   return true;
+// });
